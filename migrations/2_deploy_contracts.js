c426efd216896a87ea8ad3226eff57bcfb5e499b
Mikael Beyene <mikael@beyene.org>
2019-08-19 14:57:28 +0200

    Fix linking error

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
 const GeneDrugRepo = artifacts.require("GeneDrugRepo");
 const Encoder = artifacts.require("Encoder");
+const Util = artifacts.require("Util");
 
 module.exports = function(deployer) {
-  deployer.deploy(GeneDrugRepo);
+  deployer.deploy(Util);
+  deployer.link(Util, GeneDrugRepo);
   deployer.deploy(Encoder);
+  deployer.deploy(GeneDrugRepo);
 };
