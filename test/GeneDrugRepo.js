d74595386fa70e68077bf994824096fd4af25958
Mikael Beyene <mikael@beyene.org>
2019-08-22 19:44:58 +0200

    Add tests for wildcard key derivation

--- test/GeneDrugRepo.js
+++ test/GeneDrugRepo.js
@@ -16,10 +16,19 @@ contract("GeneDrugRepo", accounts => {
         let expectedVariantNumber = "10";
         let expectedDrugName = "antipsychotics";
 
+        await repo.insertObservation(
+            expectedGeneName,
+            10,
+            expectedDrugName,
+            "IMPROVED",
+            true,
+            true
+        );
+
         let encoded = await repo.encodeKey(expectedGeneName, expectedVariantNumber, expectedDrugName);
         let keys = await repo.deriveKeys(encoded);
 
-        ssert.equal(keys.length, 1);
+        assert.equal(keys.length, 1);
     });
 
     it("should derive keys (wildcard on gene name)", async () => {
@@ -29,20 +38,71 @@ contract("GeneDrugRepo", accounts => {
         let expectedVariantNumber = "10";
         let expectedDrugName = "antipsychotics";
 
+        let genes = ["ABCB1", "ABCC4", "ABCG2", "ABL2", "ACE", "ADD1", "ADORA2A"];
+        for (const gene of genes) {
+            await repo.insertObservation(
+                gene,
+                10,
+                expectedDrugName,
+                "IMPROVED",
+                true,
+                true
+                );
+        }
+
         let encoded = await repo.encodeKey(expectedGeneName, expectedVariantNumber, expectedDrugName);
         let keys = await repo.deriveKeys(encoded);
 
-        assert.equal(keys.length, 127);
+        assert.equal(keys.length, genes.length);
     });
 
-    /*
     it("should derive keys (wildcard on variant number)", async () => {
         const repo = await GeneDrugRepo.new();
-    });
+
+        let expectedGeneName = "ABCB1"
+        let expectedVariantNumber = "*";
+        let expectedDrugName = "antipsychotics";
+
+        let variants = [1, 2, 3, 5, 99, 98, 97];
+        for (const variant of variants) {
+            await repo.insertObservation(
+                expectedGeneName,
+                variant,
+                expectedDrugName,
+                "IMPROVED",
+                true,
+                true
+                );
+        }
+
+        let encoded = await repo.encodeKey(expectedGeneName, expectedVariantNumber, expectedDrugName);
+        let keys = await repo.deriveKeys(encoded);
+
+        assert.equal(keys.length, variants.length);
+    });  
 
     it("should derive keys (wildcard on drug name)", async () => {
         const repo = await GeneDrugRepo.new();
-    });
-    */
 
+        let expectedGeneName = "ABCB1"
+        let expectedVariantNumber = "10";
+        let expectedDrugName = "*";
+
+        let drugs = ["antipsychotics", "antidepressants", "ataluren", "diazepam"];
+        for (const drug of drugs) {
+            await repo.insertObservation(
+                expectedGeneName,
+                10,
+                drug,
+                "IMPROVED",
+                true,
+                true
+                );
+        }
+
+        let encoded = await repo.encodeKey(expectedGeneName, expectedVariantNumber, expectedDrugName);
+        let keys = await repo.deriveKeys(encoded);
+
+        assert.equal(keys.length, drugs.length);
+    });
 });
